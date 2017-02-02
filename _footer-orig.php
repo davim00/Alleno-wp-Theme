8780ab0e6ba107f55ae2ad7d68f623dd1482096f
Matthew Davis <matt.davis0207@gmail.com>
2017-02-02 17:30:15 -0500

    Backed up original PHP files

---
+++ _footer-orig.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Alleno
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'alleno-cv' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'alleno-cv' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'alleno-cv' ), 'alleno-cv', '<a href="https://automattic.com/" rel="designer">Matt Davis</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
