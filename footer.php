273ea9ce8e06cedd153b6629b946af7f03c9cf0b
Matthew Davis <matt.davis0207@gmail.com>
2017-02-15 11:25:18 -0500

    Changed URL to correct portfolio site

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		</div>
 		<div class="col-sm-12 site-info">
 			<?php echo alleno_cv_copyright(); ?>
-			<?php printf( esc_html__( ' by %1$s.', 'alleno-cv' ), '<a href="http://salmo150.com/portfolio/" rel="designer">Matt Davis</a>' ); ?>
+			<?php printf( esc_html__( ' by %1$s.', 'alleno-cv' ), '<a href="http://mattdavisdesign.com" rel="designer">Matt Davis</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 	</div><!-- .alleno-page -->
