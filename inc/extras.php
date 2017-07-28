632052a13b6c307994f95fe22a75b528afb96c6c
Matthew Davis <matt.davis0207@gmail.com>
2017-07-28 17:29:30 -0400

    Sanitize fix for nav walker

--- inc/extras.php
+++ inc/extras.php
@@ -37,3 +37,30 @@ function alleno_cv_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'alleno_cv_pingback_header' );
+
+if ( ! function_exists( 'sanitize_html_classes' ) && function_exists( 'sanitize_html_class' ) ) {
+  /**
+	 * sanitize_html_class works just fine for a single class
+	 * Some times le wild <span class="navbar-collapse collapse"> appears, which is when you need this function,
+	 * to validate both navbar-collapse and collapse,
+	 * Because sanitize_html_class doesn't allow spaces.
+	 *
+	 * @uses   sanitize_html_class
+	 * @param  (mixed: string/array) $class   'navbar-collapse collapse' or array(  'navbar-collapse', 'collapse'  )
+	 * @param  (mixed) $fallback Anything you want returned in case of a failure
+	 * @return (mixed: string / $fallback )
+	 */
+	function sanitize_html_classes( $class, $fallback = null ) {
+		// Explode it, if it's a string
+		if ( is_string( $class ) ) {
+			$class = explode( " ", $class );
+		}
+		if ( is_array( $class ) && count( $class ) > 0 ) {
+			$class = array_map( "sanitize_html_class", $class );
+			return implode( " ", $class );
+		}
+		else {
+			return sanitize_html_class( $class, $fallback );
+		}
+	}
+}
